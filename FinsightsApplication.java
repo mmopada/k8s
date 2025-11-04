6875be90a8a4aef9528124b2df66e1b1eea825f3
mmopada <142241534+mmopada@users.noreply.github.com>
2025-11-04 07:15:55 +0530

    Add files via upload

---
+++ FinsightsApplication.java
@@ -0,0 +1,11 @@
+package com.example.finsights;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinsightsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FinsightsApplication.class, args);
+    }
+}
